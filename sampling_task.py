e7aaa009308ae1c517a39c205d449e03c45166d4
Achilleas <achilleasvlogiaris@gmail.com>
2019-06-21 16:44:43 +0200

    sampling task added

---
+++ sampling_task.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import random
+import collections
+import matplotlib.pyplot as plt
+import numpy as np
+
+def frequent_IPs(dataset, num):
+
+    all_IPs=[]
+
+    for index, row in dataset.iterrows():
+        if row.Source != '147.32.84.165':
+            all_IPs.append(row.Source)
+        if row.Destination != '147.32.84.165':
+            all_IPs.append(row.Destination)
+    result = collections.Counter(all_IPs).most_common(num)
+    #return (counter.most_common(k), ips)
+    return result, all_IPs
+
+def main():
+    infected_dataset = pd.read_pickle('infected_dataset.pkl')
+    TenMostFrequentIPs, all_IPs = frequent_IPs(infected_dataset, 10)
+    print(TenMostFrequentIPs)
+
+    #distribution of ips
+    for ip in TenMostFrequentIPs:
+        print('number of connections:', ip[1], 'percentile', ip[1]*100.0/len(infected_dataset))
+
+
+
+    ###### min wise sampling
+    mse_list = []
+    for reservoir_size in range(50, 1050, 50):
+        for index, row in infected_dataset.iterrows():
+            rand = random.uniform(0, 1)
+            infected_dataset.set_value(index, 'randomNum', rand)
+
+        sorted_dataset = infected_dataset.sort_values(['randomNum'], ascending=[True])
+        sample = sorted_dataset[0:reservoir_size]
+        sample = sample.reset_index(drop=True)
+        # print(sample)
+        Min_wise_TenMostFrequentIPs, all_ip = frequent_IPs(sample, 10)
+        print('=======> Value of reservoir:', reservoir_size)
+        print('Top 10:', Min_wise_TenMostFrequentIPs)
+        for ip in Min_wise_TenMostFrequentIPs:
+            print('Element', ip, 'percentage', ip[1]*100.0/reservoir_size, '%')
+
+        # >>>>calculate mse error<<<
+        # count = 0
+        # for ip in Min_wise_TenMostFrequentIPs:
+        #     se = (ip[1] * 100.0 / reservoir_size - TenMostFrequentIPs[count][1] * 100.0 / len(infected_dataset)) ** 2
+        #     count += 1
+        # mse = (1 / 10) * se
+        # print(mse)
+        # mse_list.append(mse)
+
+    # print(mse_list)
+
+    # plt.plot(mse_list)
+    # plt.show()
+
+if __name__ == "__main__":
+        main()
